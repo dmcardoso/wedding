771dde2cce1d0b8e9a88225bd00f1c910e956bc4
Daniel Moreira Cardoso <dmcardoso.99@hotmail.com>
2023-02-20 21:23:37 -0300

    finish deploy

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+const merge = require("merge-stream");
 
 // compile scss to css
 gulp.task('sass', function () {
@@ -24,6 +25,32 @@ gulp.task('minify-js', function () {
         .pipe(uglify())
         .pipe(rename({basename: 'scripts.min'}))
         .pipe(gulp.dest('./js'));
+});
+
+// copy build
+gulp.task('copy-build-resources', function () {
+    return merge([
+        gulp.src('./img/*')
+        .pipe(gulp.dest('./build/img')),
+        gulp.src('./css/*')
+        .pipe(gulp.dest('./build/css')),
+        gulp.src('./fonts/*')
+        .pipe(gulp.dest('./build/fonts')),
+        gulp.src('./js/*')
+        .pipe(gulp.dest('./build/js')),
+        gulp.src('./index.html')
+        .pipe(gulp.dest('./build')),
+        gulp.src('./android-chrome-192x192.png').pipe(gulp.dest('./build')),
+        gulp.src('./android-chrome-512x512.png').pipe(gulp.dest('./build')),
+        gulp.src('./apple-touch-icon.png').pipe(gulp.dest('./build')),
+        gulp.src('./browserconfig.xml').pipe(gulp.dest('./build')),
+        gulp.src('./favicon-16x16.png').pipe(gulp.dest('./build')),
+        gulp.src('./favicon-32x32.png').pipe(gulp.dest('./build')),
+        gulp.src('./favicon.ico').pipe(gulp.dest('./build')),
+        gulp.src('./manifest.json').pipe(gulp.dest('./build')),
+        gulp.src('./mstile-150x150.png').pipe(gulp.dest('./build')),
+        gulp.src('./safari-pinned-tab.svg').pipe(gulp.dest('./build')),
+    ])
 });
 
 // default task
